66d8113937246a3fd433e6ef93eae585a6b639c9
Avishka Perera <avi.v.perera@icloud.com>
2018-06-07 05:54:51 +1000

    Initial Commit

---
+++ Practical 2: UIKit and SwiftTests/Practical_2__UIKit_and_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Practical_2__UIKit_and_SwiftTests.swift
+//  Practical 2: UIKit and SwiftTests
+//
+//  Created by Avi Perera on 7/6/18.
+//  Copyright © 2018 Avishka Perera. All rights reserved.
+//
+
+import XCTest
+@testable import Practical_2__UIKit_and_Swift
+
+class Practical_2__UIKit_and_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
